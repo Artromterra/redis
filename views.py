e9da7b14198f5875850d6937ab32330ce8c0f9bc
dfgaraev <56137646+dfpetrov@users.noreply.github.com>
2020-03-29 00:18:43 +0300

    Init

--- views.py
+++ views.py
@@ -29,7 +29,7 @@ def foo(number):
         fibo_list = [fibo(n) for n in cache_list]
         if cache_dict:
             cache_dict[str(number)] = str(fibo_list[-1])
-            client.hmset('cache_dict', cache_dict)
+            client.hset('cache_dict', cache_dict)
         else:
-            client.hmset('cache_dict', {str(number): str(fibo_list[-1])})
+            client.hset('cache_dict', {str(number): str(fibo_list[-1])})
         return f'посчитали: {fibo_list[-1]}<br><br>кэш: {str(cache_dict)}'
