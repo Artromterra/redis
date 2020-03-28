e0d652e3b6376df86595c6ea72cdf5d3550512cd
dfgaraev <56137646+dfpetrov@users.noreply.github.com>
2020-03-29 00:53:16 +0300

    Init

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 import pickle
 import redis
+import json
 
 client = redis.Redis(host='redis', port=6379)
 
@@ -15,23 +16,25 @@ def fibo(n):
 
 def foo(number):
 
-    cache_dict = client.hgetall('cache_d')
+    cache_dict = client.get('cache_d')
     if cache_dict and str(number) in pickle.loads(cache_dict):
-        return f'из кэша: {pickle.loads(cache_dict)[str(number)]}'
+        jdict = json.loads(cache_dict.decode("utf-8"))
+        if str(number) in jdict:
+            return f'из кэша: {jdict[str(number)]}'
+
+    if number > 500:
+        step = 500
+        k = 100
     else:
-        if number > 500:
-            step = 500
-            k = 100
-        else:
-            step = 10
-            k = 1
-        cache_list = [x*k+(step*(x-1)) for x in range(1, round(number/step)+1)]
-        cache_list.append(number)
-        fibo_list = [fibo(n) for n in cache_list]
-        if cache_dict:
-            cache_dict[str(number)] = str(fibo_list[-1])
-            # client.hset('cache_dict', 'r', cache_dict)
-            client.set('cache_d', pickle.dumps(cache_dict))
-        else:
-            client.hset('cache_d', 'r', pickle.dumps({str(number): str(fibo_list[-1])}))
-        return f'посчитали: {fibo_list[-1]}<br><br>кэш: {str(cache_dict)}'
+        step = 10
+        k = 1
+    cache_list = [x*k+(step*(x-1)) for x in range(1, round(number/step)+1)]
+    cache_list.append(number)
+    fibo_list = [fibo(n) for n in cache_list]
+    if cache_dict:
+        jdict = json.loads(cache_dict.decode("utf-8"))
+        jdict[str(number)] = str(fibo_list[-1])
+        client.set('cache_d', jdict)
+    else:
+        client.set('cache_f', str(json.dumps({str(number): str(fibo_list[-1])})))
+    return f'посчитали: {fibo_list[-1]}<br><br>кэш: {str(cache_dict)}'
