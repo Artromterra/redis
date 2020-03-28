f903b49889a4a2f05a853cc752e99b1136069466
dfgaraev <56137646+dfpetrov@users.noreply.github.com>
2020-03-28 22:38:19 +0300

    Init

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-import time
-
-import redis
+from views import foo, boo
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+# def get_hit_count():
+#     retries = 5
+#     while True:
+#         try:
+#             return cache.incr('hits')
+#         except redis.exceptions.ConnectionError as exc:
+#             if retries == 0:
+#                 raise exc
+#             retries -= 1
+#             time.sleep(0.5)
+@app.route('/', methods = ['GET'])
+@app.route('/<int:number>', methods = ['GET'])
+def show_post(number=1):
+    # return 'number %d' % number
+    return foo(number)
+
+@app.route('/rm', methods = ['GET'])
+def rm():
+    return boo()
